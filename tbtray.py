52f8928c21b83b89b69896452a48ca5d088a6d95
JackDinn <*8nw1V*@JC$NTU6P86bf>
2023-05-15 14:17:02 +0100

    Wanted thunderbird to startup minimised.
    Signed-off-by: JackDinn <*8nw1V*@JC$NTU6P86bf>

--- tbtray.py
+++ tbtray.py
@@ -6,6 +6,7 @@ import os
 import re
 import subprocess
 import sys
+import time
 import urllib.request
 from email.header import decode_header, make_header
 from pathlib import Path
@@ -31,7 +32,10 @@ def checkvisable():
     try:
         # check 3 times because sometimes when there are lots of child windows open it misses the fact that the TB window is actually visible.
         for two in range(3):
-            out = subprocess.run(["xdotool", "search", "--all", "--onlyvisible", "--maxdepth", "3", "--limit", "1", "--name", winname], stdout=subprocess.PIPE).stdout.decode('UTF-8')
+            out = subprocess.run(
+                ["xdotool", "search", "--all", "--onlyvisible", "--maxdepth", "3", "--limit", "1", "--name", winname],
+                stdout=subprocess.PIPE).stdout.decode('UTF-8')
+            log('check is TB window visable ' + out)
             if out: return True
     except:
         pass
@@ -61,18 +65,18 @@ def getfavicon(url):
         data = urllib.request.urlopen('https://www.google.com/s2/favicons?domain=' + url)
         icon = data.read()
         if icon == b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x10\x00\x00\x00\x10\x08\x06\x00\x00\x00\x1f' \
-                b'\xf3\xffa\x00\x00\x01\xb3IDATx\x01b\xf8\xff\xff?E\x18\xce\xf0\x8b\x9c\xc7\xe9\xe8;C\xb6\xaai[PI\xed\xe6\xbd\xc9\xb9' \
-                b'\xab>\x86\xc4\x03\xe8\xa6\x86\xed\xda\x82(\xf81\xefc\xde\xf4b\x18\xdb\xb6\xedd\x1a\xdb\xb6m\xdb\xb6m\';]\x83\xee\xd5W{' \
-                b'\xad\xe3\xaa\xda\xaaS\xf8\x83\xc3\xd2\xb9\xf2\xc1+\xb8\xa9\'\x80}\x03\x06X\xce\x13\xe4\xff\xaa\xac\x7f.~\xf5\xf1\x16N\x15' \
-                b'\xab1\xc9\xdd\xd7E\xb5\xb3\x94S1E\xb8\x0eO\x1fPX\\\xc7\xb5\x99C\xf9\xaa\x8bo]<\xb0\xe0\x08\x01\xa8\xba\xf8\xd6\xc7[\xb9T\x1d' \
-                b'\xa6d\x0cRm\xfb\nu\x0cn\xd1\xd5\xdd\x1b\xed\x9f<\x10\xe2\xf4\xf2\x85\xf0\x8e\x89\x1c:\xfb\xd4\xc5\x83#\x04P\x9a\x99c\xf9*2\x81' \
-                b'\x8c\xa3\xa9g\x1d\x87\x10\xf8\xfe!\xea\x1d\xdb\xa5>v\x98:\x94\xad\x82#\x04\xd0\x1fJ\x04\x08d\xf9xx\xfe"\x04\xaex\x9e_=\xa3\xe0' \
-                b'\xe8\xf6k\xcf\xa0\xc6\x1e!\x80!!;@\xfa\xc8\xbc\x82\x95\xad+\xc2{`\xcd\x1d+\x1e\x84\x80\xda\xb4\xe0gk\xff\x8e\x10\xe8\x13@N\x96' \
-                b'\x03-@\x00X\x95q\xfe\x8f\x86\x00\x86\xc4K\xe5\x03\xd4\'\x80\xb9@@)\x0b\xb0\xd5\xa1\x05\x92\x03\xcf\xfc\xe0\xe2\x08&\x8cw\xd8\xc6' \
-                b'\x83\xc6\x10\xc3\xd9\x10%\xbe\xce<&\x16N\x88\x87_x\xcb\xb5G`c\x8f\xbcF%L\x02eC\x0280H`L\xec\xd9\x1a#Z\x95\x1aF\x82\xc3\x98\x17\x0ee' \
-                b'\x11\xf4\xca\xc9\xb8\x1f\x06\xd9\xaeL\xd7H\xdc\xca\xceL\x04&a\xff\xc35\xc00\r\xec\x9cV4N\x81\x91m\xd7\xc8\x0c\xb2\x8e\x95\xe5' \
-                b'\x9f\t\xa5\xc1$\xd8\xb3\xca\xa4\xe0\x07\xd3\xfe\x1b(\xc0\x80b{\xaa\x9b\xb7\x078a\xc9L\x14a\x00BM\xf5\xdf\xed\xe70\xb1\x00\x00' \
-                b'\x00\x00IEND\xaeB`\x82':
+                   b'\xf3\xffa\x00\x00\x01\xb3IDATx\x01b\xf8\xff\xff?E\x18\xce\xf0\x8b\x9c\xc7\xe9\xe8;C\xb6\xaai[PI\xed\xe6\xbd\xc9\xb9' \
+                   b'\xab>\x86\xc4\x03\xe8\xa6\x86\xed\xda\x82(\xf81\xefc\xde\xf4b\x18\xdb\xb6\xedd\x1a\xdb\xb6m\xdb\xb6m\';]\x83\xee\xd5W{' \
+                   b'\xad\xe3\xaa\xda\xaaS\xf8\x83\xc3\xd2\xb9\xf2\xc1+\xb8\xa9\'\x80}\x03\x06X\xce\x13\xe4\xff\xaa\xac\x7f.~\xf5\xf1\x16N\x15' \
+                   b'\xab1\xc9\xdd\xd7E\xb5\xb3\x94S1E\xb8\x0eO\x1fPX\\\xc7\xb5\x99C\xf9\xaa\x8bo]<\xb0\xe0\x08\x01\xa8\xba\xf8\xd6\xc7[\xb9T\x1d' \
+                   b'\xa6d\x0cRm\xfb\nu\x0cn\xd1\xd5\xdd\x1b\xed\x9f<\x10\xe2\xf4\xf2\x85\xf0\x8e\x89\x1c:\xfb\xd4\xc5\x83#\x04P\x9a\x99c\xf9*2\x81' \
+                   b'\x8c\xa3\xa9g\x1d\x87\x10\xf8\xfe!\xea\x1d\xdb\xa5>v\x98:\x94\xad\x82#\x04\xd0\x1fJ\x04\x08d\xf9xx\xfe"\x04\xaex\x9e_=\xa3\xe0' \
+                   b'\xe8\xf6k\xcf\xa0\xc6\x1e!\x80!!;@\xfa\xc8\xbc\x82\x95\xad+\xc2{`\xcd\x1d+\x1e\x84\x80\xda\xb4\xe0gk\xff\x8e\x10\xe8\x13@N\x96' \
+                   b'\x03-@\x00X\x95q\xfe\x8f\x86\x00\x86\xc4K\xe5\x03\xd4\'\x80\xb9@@)\x0b\xb0\xd5\xa1\x05\x92\x03\xcf\xfc\xe0\xe2\x08&\x8cw\xd8\xc6' \
+                   b'\x83\xc6\x10\xc3\xd9\x10%\xbe\xce<&\x16N\x88\x87_x\xcb\xb5G`c\x8f\xbcF%L\x02eC\x0280H`L\xec\xd9\x1a#Z\x95\x1aF\x82\xc3\x98\x17\x0ee' \
+                   b'\x11\xf4\xca\xc9\xb8\x1f\x06\xd9\xaeL\xd7H\xdc\xca\xceL\x04&a\xff\xc35\xc00\r\xec\x9cV4N\x81\x91m\xd7\xc8\x0c\xb2\x8e\x95\xe5' \
+                   b'\x9f\t\xa5\xc1$\xd8\xb3\xca\xa4\xe0\x07\xd3\xfe\x1b(\xc0\x80b{\xaa\x9b\xb7\x078a\xc9L\x14a\x00BM\xf5\xdf\xed\xe70\xb1\x00\x00' \
+                   b'\x00\x00IEND\xaeB`\x82':
             os.symlink(os.getcwd() + '/res/thunderbird.png', iconpath)
             return 'res/thunderbird.png'
         with open(iconpath, "wb") as f:
@@ -86,7 +90,8 @@ def getfavicon(url):
 def checksettings():
     my_dir = Path(str(Path.home()) + '/.config/tbtray')
     if not my_dir.is_dir(): my_dir.mkdir()
-    if not Path(str(Path.home()) + '/.config/tbtray/icons').is_dir(): Path.mkdir(Path(str(Path.home()) + '/.config/tbtray/icons'))
+    if not Path(str(Path.home()) + '/.config/tbtray/icons').is_dir(): Path.mkdir(
+        Path(str(Path.home()) + '/.config/tbtray/icons'))
     my_file = Path(str(Path.home()) + '/.config/tbtray/settings.ini')
     if not my_file.is_file(): copyfile('settings.ini', str(my_file))
     config_new = configparser.ConfigParser()
@@ -111,24 +116,30 @@ def readmessage(path):
     date_text = []
     messageid_text = []
     for gg in path:
-        if not os.path.isfile(gg): continue
-        text = subprocess.run(["tail", "-n", "10000", gg], stdout=subprocess.PIPE).stdout.decode('UTF-8', "ignore")
-        with open('/tmp/tbtraydata', 'w+') as xyz:
-            xyz.write(text)
-        fr = mailbox.mbox('/tmp/tbtraydata')
-        if os.path.isfile('/tmp/tbtraydata'): os.remove('/tmp/tbtraydata')
-        for q in fr:
-            try:
-                messageid_text.append(str(make_header(decode_header(q['message-ID']))))
-                date_text.append(str(make_header(decode_header(q['date']))))
-                from_text.append(str(make_header(decode_header(q['from']))).replace('<', '&lt;').replace('>', '&gt;'))
-                subject_text.append(str(make_header(decode_header(q['subject']))).replace('\r\n', '<br>'))
-            except:
-                continue
+        try:
+            if not os.path.isfile(gg): continue
+            text = subprocess.run(["tail", "-n", "10000", gg], stdout=subprocess.PIPE).stdout.decode('UTF-8', "ignore")
+            with open('/tmp/tbtraydata', 'w+') as xyz:
+                xyz.write(text)
+            fr = mailbox.mbox('/tmp/tbtraydata')
+            if os.path.isfile('/tmp/tbtraydata'): os.remove('/tmp/tbtraydata')
+            for q in fr:
+                try:
+                    messageid_text.append(str(make_header(decode_header(q['message-ID']))))
+                    date_text.append(str(make_header(decode_header(q['date']))))
+                    from_text.append(
+                        str(make_header(decode_header(q['from']))).replace('<', '&lt;').replace('>', '&gt;'))
+                    subject_text.append(str(make_header(decode_header(q['subject']))).replace('\r\n', '<br>'))
+                except:
+                    continue
+        except:
+            continue
     return {'from': from_text, 'subject': subject_text, 'date': date_text, 'messageid': messageid_text}
 
 
 class TextBrowser(QtWidgets.QTextBrowser):
+    windowid: int
+
     def __init__(self, parent=None):
         super(TextBrowser, self).__init__(parent)
         self.windowid = 0
@@ -158,6 +169,7 @@ class TextBrowser(QtWidgets.QTextBrowser):
 
 class Popup(QtWidgets.QDialog):
 
+    # noinspection PyArgumentList
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setObjectName("formpopup")
@@ -211,7 +223,8 @@ class Popup(QtWidgets.QDialog):
                         mailinfo['subject'].pop(up)
                         mailinfo['date'].pop(up)
                         mailinfo['messageid'].pop(up)
-                    else: up += 1
+                    else:
+                        up += 1
                 for fr in mailinfo['messageid']: self.shownmessages.append(fr)
                 if not firstrun and len(mailinfo['messageid']) > 0:
                     if not self.isVisible():
@@ -228,7 +241,8 @@ class Popup(QtWidgets.QDialog):
                             if len(fav) > 0:
                                 log('get favicon ' + fav[0][0])
                                 icon = getfavicon(fav[0][0])
-                            else: icon = 'res/thunderbird.png'
+                            else:
+                                icon = 'res/thunderbird.png'
                             self.browsertext += '<h3 style="color: DodgerBlue"><img height="20" width="20" src="' + icon + '"/>&nbsp;&nbsp;' + fromx + '</h3><p>' + subject
                         else:
                             self.browsertext += '<h3 style="color: DodgerBlue"><center>' + fromx + '</center></h3><p>' + subject
@@ -236,8 +250,13 @@ class Popup(QtWidgets.QDialog):
                     if self.soundon: self.sound.play()
                     self.textBrowser.clear()
                     self.textBrowser.setText(self.browsertext)
-                    if self.top: self.setGeometry(self.xpos - self.textBrowser.width, 40, self.textBrowser.width + 20, self.textBrowser.height + 15)
-                    else: self.setGeometry(self.xpos - self.textBrowser.width, self.screenheight - 55 - self.textBrowser.height, self.textBrowser.width + 20, self.textBrowser.height + 15)
+                    if self.top:
+                        self.setGeometry(self.xpos - self.textBrowser.width, 40, self.textBrowser.width + 20,
+                                         self.textBrowser.height + 15)
+                    else:
+                        self.setGeometry(self.xpos - self.textBrowser.width,
+                                         self.screenheight - 55 - self.textBrowser.height, self.textBrowser.width + 20,
+                                         self.textBrowser.height + 15)
                     self.closebutton.setGeometry(self.textBrowser.width - 4, 8, 20, 20)
                     self.popup_timer.start(self.duration * 1000)
                     self.popup_timer2.start()
@@ -288,7 +307,6 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
         self.timetriggercheck = QTimer(self)
         self.tray_icon = QSystemTrayIcon(self)
         self.INTRAY = False
-        # self.winclass = ' - Mozilla Thunderbird'
         self.windowid = 0
         self.setupUi(self)
         self.profiles = []
@@ -365,7 +383,8 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
         self.toolButton_notifyicon.clicked.connect(self.func_notifyicon)
         self.pushButton_colourpicker.clicked.connect(self.func_colourpicker)
         self.tray_icon.show()
-        if self.badprofile: self.tray_icon.showMessage('TBtray Profile Warning', 'Please setup account profiles', QSystemTrayIcon.Critical)
+        if self.badprofile: self.tray_icon.showMessage('TBtray Profile Warning', 'Please setup account profiles',
+                                                       QSystemTrayIcon.Critical)
         self.popup.fire(self.profiles, True)
 
     def func_toolbutton_firepopup(self):
@@ -381,12 +400,19 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
         if self.checkBox_popup.isChecked(): self.popup_test.show()
         self.popup_test.setWindowOpacity(float(self.horizontalSlider_opacity.value() / 100))
         self.popup_test.textBrowser.fixedwidth = self.checkBox_fixedwidth.isChecked()
-        if self.popup_test.textBrowser.fixedwidth: self.popup_test.textBrowser.setLineWrapMode(QtWidgets.QTextBrowser.WidgetWidth)
-        if not self.popup_test.textBrowser.fixedwidth: self.popup_test.textBrowser.setLineWrapMode(QtWidgets.QTextBrowser.NoWrap)
+        if self.popup_test.textBrowser.fixedwidth: self.popup_test.textBrowser.setLineWrapMode(
+            QtWidgets.QTextBrowser.WidgetWidth)
+        if not self.popup_test.textBrowser.fixedwidth: self.popup_test.textBrowser.setLineWrapMode(
+            QtWidgets.QTextBrowser.NoWrap)
         self.popup_test.textBrowser.clear()
         self.popup_test.textBrowser.setText(self.popup_test.browsertext)
-        if self.radioButton_top.isChecked(): self.popup_test.setGeometry(self.spinBox_xpos.value() - self.popup_test.textBrowser.width, 40, self.popup_test.textBrowser.width + 20, self.popup_test.textBrowser.height + 15)
-        else: self.popup_test.setGeometry(self.spinBox_xpos.value() - self.popup_test.textBrowser.width, self.popup_test.screenheight - 55 - self.popup_test.textBrowser.height, self.popup_test.textBrowser.width + 20, self.popup_test.textBrowser.height + 15)
+        if self.radioButton_top.isChecked():
+            self.popup_test.setGeometry(self.spinBox_xpos.value() - self.popup_test.textBrowser.width, 40,
+                                        self.popup_test.textBrowser.width + 20, self.popup_test.textBrowser.height + 15)
+        else:
+            self.popup_test.setGeometry(self.spinBox_xpos.value() - self.popup_test.textBrowser.width,
+                                        self.popup_test.screenheight - 55 - self.popup_test.textBrowser.height,
+                                        self.popup_test.textBrowser.width + 20, self.popup_test.textBrowser.height + 15)
         self.popup_test.closebutton.setGeometry(self.popup_test.textBrowser.width - 4, 8, 20, 20)
         self.popup_test.popup_timer.start(self.spinBox_displaytime.value() * 1000)
         self.popup_test.popup_timer2.start()
@@ -403,11 +429,13 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
             self.colour_pre = x.name()
 
     def func_defaulticon(self):
-        x = QFileDialog.getOpenFileName(self, 'Select Default Icon', 'res/', "Icons .png .ico .svg (*.png *.ico *.svg)")[0]
+        x = \
+        QFileDialog.getOpenFileName(self, 'Select Default Icon', 'res/', "Icons .png .ico .svg (*.png *.ico *.svg)")[0]
         if x: self.lineedit_defulticon.setText(x)
 
     def func_notifyicon(self):
-        x = QFileDialog.getOpenFileName(self, 'Select Notify Icon', 'res/', "Icons .png .ico .svg (*.png *.ico *.svg)")[0]
+        x = QFileDialog.getOpenFileName(self, 'Select Notify Icon', 'res/', "Icons .png .ico .svg (*.png *.ico *.svg)")[
+            0]
         if x: self.lineedit_notifyicon.setText(x)
 
     def func_minimizetotrayclicked(self):
@@ -443,7 +471,9 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
         self.timetriggercheck.start(1000)
 
     def selectfile(self):
-        x = QFileDialog.getOpenFileName(self, 'Select Profile .msf File', '/home/' + getpass.getuser() + '/.thunderbird/', "INBOX.msf(INBOX.msf)")[0]
+        x = \
+        QFileDialog.getOpenFileName(self, 'Select Profile .msf File', '/home/' + getpass.getuser() + '/.thunderbird/',
+                                    "INBOX.msf(INBOX.msf)")[0]
         if x: self.editline_profilepath.setText(x)
 
     def cancel(self):
@@ -563,7 +593,8 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
             self.label_accountwarrning.setStyleSheet('color: red')
             self.label_accountwarrning.show()
             self.tabWidget.setCurrentIndex(1)
-            self.tray_icon.showMessage('TBtray Profile Warning', 'Please setup account profiles', QSystemTrayIcon.Critical)
+            self.tray_icon.showMessage('TBtray Profile Warning', 'Please setup account profiles',
+                                       QSystemTrayIcon.Critical)
             self.timetriggercheck.start(15000)
             return
         self.timetriggercheck.stop()
@@ -573,11 +604,13 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
             self.popup_test.textBrowser.INTRAY = False
         if not self.windowid:
             stdout = subprocess.run(["wmctrl", '-lx'], stdout=subprocess.PIPE).stdout.decode('UTF-8')
-            idx = re.findall('(\dx\w+)..0 Mail\.Thunderbird', str(stdout))
+            idx = re.findall('(\dx\w+)..0 Mail\.thunderbird', str(stdout))
             if idx:
                 self.windowid = idx[0]
                 self.popup.textBrowser.windowid = self.windowid
                 log('grabbed window ' + idx[0])
+                subprocess.run(["xdotool", "windowunmap", self.windowid])
+                self.INTRAY = True
         if self.checkbox_minimizetotray.isChecked() and not self.INTRAY:
             if not checkvisable() and self.windowid:
                 subprocess.run(['wmctrl', '-i', '-r', str(self.windowid), '-b', 'add,skip_taskbar'])
@@ -588,7 +621,8 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
                 self.matches = 0
                 for profile2 in self.profiles:
                     if not os.path.isfile(profile2): continue
-                    filetext = subprocess.run(["tail", "-n", "2000", profile2], stdout=subprocess.PIPE).stdout.decode('UTF-8')
+                    filetext = subprocess.run(["tail", "-n", "2000", profile2], stdout=subprocess.PIPE).stdout.decode(
+                        'UTF-8')
                     matchesx = re.findall('\^A2=(\w+)', filetext)
                     if matchesx: self.matches += int(matchesx[-1], 16)
                 if self.matches > 0:
@@ -607,12 +641,15 @@ class MainApp(QtWidgets.QDialog, tbtrayui.Ui_Form):
                         painter.setOpacity(1.0)
                         painter.setPen(QColor(self.colour))
                         fm = QFontMetrics(font)
-                        painter.drawText(int((pixmap.width() - fm.width(count)) / 2), int((pixmap.height() - fm.height()) / 2 + fm.ascent() + 1), count)
+                        painter.drawText(int((pixmap.width() - fm.width(count)) / 2),
+                                         int((pixmap.height() - fm.height()) / 2 + fm.ascent() + 1), count)
                         painter.end()
                         self.tray_icon.setIcon(QtGui.QIcon(pixmap))
-                    else: self.tray_icon.setIcon(QtGui.QIcon(self.notifyicon))
+                    else:
+                        self.tray_icon.setIcon(QtGui.QIcon(self.notifyicon))
                     if not self.badprofile: self.popup.fire(self.profiles)
-                else: self.tray_icon.setIcon(QtGui.QIcon(self.defaulticon))
+                else:
+                    self.tray_icon.setIcon(QtGui.QIcon(self.defaulticon))
                 break
         self.timetriggercheck.start(1000)
 
